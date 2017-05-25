b9ef089b65868a13a2277a8f00369f9069de9172
Angel Faryshta Guevara <angeldelcaos@gmail.com>
2017-05-25 14:03:55 -0500

    role -> permission

--- src/migrations/m170101_000004_workflow_transition_permission.php
+++ src/migrations/m170101_000004_workflow_transition_permission.php
@@ -1,24 +1,33 @@
 <?php
 
-class m170101_000004_workflow_transition_role
+class m170101_000004_workflow_transition_permission
     extends \tecnocen\workflow\\migrations\BaseTable
 {
+    /**
+     * @inhertidoc
+     */
     public function getTableName()
     {
-        return 'tecnocen_workflow_transition_role';
+        return 'tecnocen_workflow_transition_permission';
     }
 
+    /**
+     * @inhertidoc
+     */
     public function columns()
     {
         return [
             'source_stage_id' => $this->normalKey(),
             'target_stage_id' => $this->normalKey(),
-            'role' => $this->string()->notNull(),
+            'permission' => $this->string()->notNull(),
         ];
     }
 
+    /**
+     * @inhertidoc
+     */
     public function compositePrimaryKeys()
     {
-        return ['source_stage_id', 'target_stage_id', 'role'];
+        return ['source_stage_id', 'target_stage_id', 'permission'];
     }
 }
