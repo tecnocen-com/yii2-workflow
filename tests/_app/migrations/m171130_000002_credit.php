37b67b55e52720d4bba0fe1c76a8c5e3949b75b9
Carlos Llamosas <carlos@tecnocen.com>
2017-12-01 16:19:14 -0600

    Add required migrations for test WorkLog

---
+++ tests/_app/migrations/m171130_000002_credit.php
@@ -0,0 +1,24 @@
+<?php
+
+class m171130_000002_credit extends \tecnocen\migrate\CreateTableMigration
+{
+    public function getTableName()
+    {
+        return 'credit';
+    }
+
+    public function columns()
+    {
+         return [
+             'id' => $this->primaryKey(),
+             'workflow_id' => $this->normalKey(),
+         ];
+    }
+
+    public function foreignKeys()
+    {
+        return [
+            'workflow_id' => ['table' => 'tecnocen_workflow']
+        ];
+    }
+}
---
+++ tests/_app/migrations/m171130_000003_credit_worklog.php
@@ -0,0 +1,9 @@
+<?php
+
+class m171130_000003_credit_worklog extends \tecnocen\workflow\migrations\WorkLog
+{
+    public function getProcessTableName()
+    {
+        return 'credit';
+    }
+}
