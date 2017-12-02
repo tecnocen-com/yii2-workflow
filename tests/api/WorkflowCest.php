45b2e45914d16cd58f563c73835e25b145fd3ef8
Carlos Llamosas <carlos@tecnocen.com>
2017-12-02 11:16:29 -0600

    Add create update delete method

--- tests/api/WorkflowCest.php
+++ tests/api/WorkflowCest.php
@@ -22,7 +22,9 @@ class WorkflowCest extends \tecnocen\roa\test\AbstractResourceCest
     }
 
     /**
-     * @dataprovider indexData
+     * @param  ApiTester $I
+     * @param  Example $example
+     * @dataprovider indexDataProvider
      * @depends fixtures
      * @before authToken
      */
@@ -32,7 +34,10 @@ class WorkflowCest extends \tecnocen\roa\test\AbstractResourceCest
         $this->internalIndex($I, $example);
     }
 
-    protected function indexData()
+    /**
+     * @return array for test `index()`.
+     */
+    protected function indexDataProvider()
     {
         return [
             [
@@ -41,6 +46,151 @@ class WorkflowCest extends \tecnocen\roa\test\AbstractResourceCest
         ];
     }
 
+    /**
+     * @param  ApiTester $I
+     * @param  Example $example
+     * @dataprovider viewDataProvider
+     * @depends fixtures
+     * @before authToken
+     */
+    public function view(ApiTester $I, Example $example)
+    {
+        $I->wantTo('Retrieve Workflow single record.');
+        $this->internalView($I, $example);
+    }
+
+    /**
+     * @return array data for test `view()`.
+     */
+    protected function viewDataProvider()
+    {
+        return [
+            'filter by name' => [
+                'urlParams' => ['name' => '1'],
+                'httpCode' => HttpCode::OK,
+                'headers' => [
+                    'X-Pagination-Total-Count' => 1,
+                ],
+            ],
+        ];
+    }
+
+    /**
+     * @param  ApiTester $I
+     * @param  Example $example
+     * @dataprovider createDataProvider
+     * @depends fixtures
+     * @before authToken
+     */
+    public function create(ApiTester $I, Example $example)
+    {
+        $I->wantTo('Create a Workflow record.');
+        $this->internalCreate($I, $example);
+    }
+
+    /**
+     * @return array data for test `create()`.
+     */
+    protected function createDataProvider()
+    {
+        return [
+            'create workflow 3' => [
+                'data' => ['name' => 'workflow 3'],
+                'httpCode' => HttpCode::CREATED,
+            ],
+            'unique' => [
+                'data' => ['name' => 'workflow 3'],
+                'httpCode' => HttpCode::UNPROCESSABLE_ENTITY,
+                'validationErrors' => [
+                    'name' => 'Workflow name "workflow 3" has already been taken.'
+                ],
+            ],
+            'to short' => [
+                'data' => ['name' => 'wo'],
+                'httpCode' => HttpCode::UNPROCESSABLE_ENTITY,
+                'validationErrors' => [
+                    'name' => 'Workflow name should contain at least 6 characters.'
+                ],
+            ],
+            'not blank' => [
+                'httpCode' => HttpCode::UNPROCESSABLE_ENTITY,
+                'validationErrors' => [
+                    'name' => 'Workflow name cannot be blank.'
+                ],
+            ],
+        ];
+    }
+
+    /**
+     * @param  ApiTester $I
+     * @param  Example $example
+     * @dataprovider updateDataProvider
+     * @depends fixtures
+     * @before authToken
+     */
+    public function update(ApiTester $I, Example $example)
+    {
+        $I->wantTo('Update a Workflow record.');
+        $this->internalUpdate($I, $example);
+    }
+
+    /**
+     * @return array data for test `update()`.
+     */
+    protected function updateDataProvider()
+    {
+        return [
+            'update workflow 1' => [
+                'urlParams' => ['id' => '1'],
+                'data' => ['name' => 'workflow 7'],
+                'httpCode' => HttpCode::OK,
+            ],
+            'to short' => [
+                'urlParams' => ['id' => '1'],
+                'data' => ['name' => 'wo'],
+                'httpCode' => HttpCode::UNPROCESSABLE_ENTITY,
+                'validationErrors' => [
+                    'name' => 'Workflow name should contain at least 6 characters.'
+                ],
+            ],
+        ];
+    }
+
+    /**
+     * @param  ApiTester $I
+     * @param  Example $example
+     * @dataprovider deleteDataProvider
+     * @depends fixtures
+     * @before authToken
+     */
+    public function delete(ApiTester $I, Example $example)
+    {
+        $I->wantTo('Delete a Workflow record.');
+        $this->internalDelete($I, $example);
+    }
+
+    /**
+     * @return array data for test `delete()`.
+     */
+    protected function deleteDataProvider()
+    {
+        return [
+            'delete workflow 1' => [
+                'urlParams' => ['id' => '1'],
+                'httpCode' => HttpCode::NO_CONTENT,
+            ],
+            'not found' => [
+                'urlParams' => ['id' => '1'],
+                'httpCode' => HttpCode::NOT_FOUND,
+                'validationErrors' => [
+                    'name' => 'The record "1" does not exists.'
+                ],
+            ],
+        ];
+    }
+    /**
+     * @inheritdoc
+     */
     protected function recordJsonType()
     {
         return [
@@ -49,6 +199,9 @@ class WorkflowCest extends \tecnocen\roa\test\AbstractResourceCest
         ];
     }
 
+    /**
+     * @inheritdoc
+     */
     protected function getRoutePattern()
     {
         return 'workflow';
