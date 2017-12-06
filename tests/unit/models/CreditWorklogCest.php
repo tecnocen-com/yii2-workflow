332c435c03858317c326d3a8b6df54b09d480dcf
Carlos Llamosas <carlos@tecnocen.com>
2017-12-05 19:23:41 -0600

    Add CreditWorklogCest

---
+++ tests/unit/models/CreditWorklogCest.php
@@ -0,0 +1,38 @@
+<?php
+
+use app\fixtures\TransitionFixture;
+use app\models\CreditWorklog;
+
+
+class CreditWorklogCest 
+{
+
+    public function fixtures(ApiTester $I)
+    {
+        $I->haveFixtures([
+            'transition' => TransitionFixture::class,
+        ]);
+    }
+
+    public function credit()
+    {
+        $creditWorklog = CreditWorklog::create();
+
+        $creditWorklog->process_id = 1;
+        $this->assertTrue($creditWorklog->validate(['process_id']));
+
+        $creditWorklog->stage_id = 2;
+        $this->assertTrue($creditWorklog->validate(['stage_id']));
+
+    }
+
+    public function save()
+    {
+        $creditWorklog = new CreditWorklog();
+        $creditWorklog->process_id = 1;
+        $creditWorklog->stage_id= 2;
+        $creditWorklog->save();
+        $this->tester->seeInDatabase('credit_worklog', 
+            ['process_id' => 1, 'stage_id' => 2]);
+    }
+}
