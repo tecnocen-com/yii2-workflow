49bcef4964c55a9efef63262a60256452dd47c99
Carlos Llamosas <carlos@tecnocen.com>
2017-12-05 19:22:59 -0600

    Mock model Credit

---
+++ tests/_app/models/Credit.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace app\models;
+
+use Yii;
+
+/**
+ * This is the model class for table `{{%credit}}`.
+ *
+ * @property integer $id
+ * @property string $credit
+ */
+class Credit extends ActiveRecord
+{
+    /**
+     * @inheritdoc
+     */
+    public static function tableName()
+    {
+        return '{{%credit}}';
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function rules()
+    {
+        return [
+            [['workflow_id'], 'required'],
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => Yii::t('app', 'ID'),
+            'workflow_id' => Yii::t('app', 'Workflow ID'),
+        ];
+    }
+
+    /**
+     * @return ActiveQuery
+     */
+    public function getWorkflow()
+    {
+        return $this->hasOne(Workflow::class, ['id' => 'workflow_id']);
+    }
+}
