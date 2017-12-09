622fc1d828c085eadc67b1a0692c7ca13512c2b7
Carlos Llamosas <carlos@tecnocen.com>
2017-12-08 22:00:11 -0600

    Module as CreditResource

--- tests/_app/config/test.php
+++ tests/_app/config/test.php
@@ -3,7 +3,7 @@
 use tecnocen\workflow\roa\modules\Version as WorkflowVersion;
 use tecnocen\roa\controllers\ProfileResource;
 use tecnocen\roa\hal\JsonResponseFormatter;
-use app\api\resource\CreditResource as CreditResource;
+use app\api\modules\Version as CreditResource;
 use yii\web\Response;
 
 return [
