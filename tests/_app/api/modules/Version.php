124cf5fbd7d8c701aabbec32c652929d831a4d9b
Carlos Llamosas <carlos@tecnocen.com>
2017-12-09 12:59:57 -0600

    Configure Version v1 for worklog

--- tests/_app/api/modules/Version.php
+++ tests/_app/api/modules/Version.php
@@ -3,15 +3,20 @@
 namespace app\api\modules;
 
 use app\api\resources\CreditResource;
+use app\api\resources\CreditWorklogResource;
 
 class Version extends \tecnocen\roa\modules\ApiVersion
 {
     const CREDIT_ROUTE = 'credit';
-    
+    const WORKLOG_ROUTE = self::CREDIT_ROUTE . '/<credit_id:\d+>/worklog';
+
     /**
      * @inheritdoc
      */
     public $resources = [
         self::CREDIT_ROUTE => ['class' => CreditResource::class],
+        self::WORKLOG_ROUTE => [
+            'class' => CreditWorklogResource::class,
+        ]
     ];
 }
