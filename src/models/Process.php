42b3daa6f0da86c504003e5d6ea59e1b9f3c429b
Angel Guevara <angeldelcaos@gmail.com>
2017-12-14 15:05:15 -0600

    Process class

---
+++ src/models/Process.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace tecnocen\workflow\models;
+
+use tecnocen\rmdb\models\Entity;
+use yii\base\InvalidConfigException;
+
+/**
+ * Model class for process which change stages depending on a worklow
+ */
+abstract class Process extends Entity
+{
+    protected abstract function workLogClass();
+
+    public abstract function getWorkflowId();
+
+    public function init()
+    {
+        if (!is_subclass_of($this->workLogClass(), WorkLog::class)) {
+            throw new InvalidConfigException(
+                static::class . '::workLogClass() must extend '
+                    . WorkLog::class
+            );
+        }
+        parent::init();
+    }
+
+    public function getWorkLogs()
+    {
+        return $this->hasMany($this->workLogClass(), [
+            'process_id' => 'id',
+        ]);
+    }
+
+    public function getActiveWorkLog()
+    {
+        $query = $this->getWorkLogs()->alias('worklog');
+        $query->multiple = false;
+        $workLogClass = $this->workLogClass();
+
+        return $query->andWhere([
+            'created_at' => $workLogClass()::find()
+                ->select(['MAX(created_at)'])
+                ->alias('worklog_groupwise')
+                ->andWhere('worklog.process_id = worklog_groupwise.process_id')
+        ])->groupBy(['worklog.process_id']);
+    }
+
+    public function flow(&$workLog)
+    {
+        $workLog = $this->ensureWorkLog($workLog);
+        $workLog->scenario = WorkLog::SCENARIO_FLOW;
+
+        return $workLog->save();
+    }
+
+    public function initialLog(&$workLog)
+    {
+        $workLog = $this->ensureWorkLog($workLog);
+        $workLog->scenario = WorkLog::SCENARIO_INITIAL;
+
+        return $workLog->save();
+    }
+
+    private function ensureWorkLog($workLog)
+    {
+        $workLogClass = $this->workLogClass();
+        if (is_array($workLog)) {
+            $workLog = new $workLogClass($workLog);
+        } elseif (!$workLog instanceof $workLogClass) {
+            throw new InvalidParamException(
+                "Parameter must be an instance of {$workLogClass} or an "
+                    . 'array to configure an instance'
+            );
+        }
+
+        $workLog->process_id = $this->id;
+        $workLog->setRelation('process', $this);
+
+        return $workLog;
+    }
+}
--- src/models/WorkLog.php
+++ src/models/WorkLog.php
@@ -2,24 +2,24 @@
 
 namespace tecnocen\workflow\models;
 
+use tecnocen\rmdb\models\Pivot;
+
 /**
- * Model class for table `{{%workflow}}`
- *
- * @property integer $source_stage_id
- * @property integer $target_stage_id
+ * @property integer $process_id
+ * @property integer $stage_id
  * @property string $name
  *
  * @property ActiveRecord $process
  */
-abstract class WorkLog extends \yii\db\ActiveRecord
+abstract class WorkLog extends Pivot
 {
+    const SCENARIO_INITIAL = 'initial';
+    const SCENARIO_FLOW = 'flow';
+
     /**
      * @return string class name for the process this worklog is attached to.
      */
-    public static function processClass()
-    {
-        return '';
-    }
+    protected abstract function processClass();
 
     /**
      * @inheritdoc
@@ -27,47 +27,43 @@ abstract class WorkLog extends \yii\db\ActiveRecord
     public function rules()
     {
         return [
-            [['process_id', 'stage_id'], 'required'],
-            [['process_id', 'stage_id'], 'integer'],
+            [['process_id'], 'required', 'on' => [self::SCENARIO_DEFAULT]],
+            [['process_id'], 'integer', 'on' => [self::SCENARIO_DEFAULT]],
+            [['stage_id'], 'required'],
+            [['stage_id'], 'integer'],
             [
                 ['process_id'],
                 'exist',
                 'targetAttribute' => ['process_id' => 'id'],
-                'targetClass' => static::processClass(),
+                'targetClass' => $this->processClass(),
+                'on' => [self::SCENARIO_DEFAULT],
             ],
             [
                 ['stage_id'],
                 'exist',
                 'targetAttribute' => ['stage_id' => 'id'],
                 'targetClass' => Stage::class,
+                'filter' => function ($query) {
+                    $query->andWhere(['initial' => true]);
+                },
+                'on' => [self::SCENARIO_INITIAL],
             ],
             [
                 ['stage_id'],
                 'exist',
-                'targetAttribute' => ['stage_id' => 'id'],
-                'targetClass' => Stage::class,
+                'targetAttribute' => ['stage_id' => 'target_stage_id'],
+                'targetClass' => Transition::class,
+                'filter' => function ($query) {
+                    $query->andWhere([
+                        'source_stage_id' => $this->process->activeWorkLog
+                            ->stage_id
+                    ]);
+                },
                 'when' => function () {
                     return !$this->hasErrors('process_id')
                         && null === $this->process->currentStage;
                 },
-                'filter' => function ($query) {
-                    $query->andWhere(['initial' => true]);
-                },
-            ],
-            [
-                ['stage_id'],
-                'exist',
-                'targetClass' => Transition::class,
-                'targetAttribute' => ['stage_id' => 'target_stage_id'],
-                'when' => function () {
-                    return !$this->hasErrors('process_id')
-                        && null !== $this->process->currentStage;
-                },
-                'filter' => function ($query) {
-                    $query->andWhere([
-                        'source_stage_id' => $this->process->currentStage->id
-                    ]);
-                },
+                'on' => [self::SCENARIO_FLOW],
                 'message' => 'There is no transition for the current stage'
             ],
         ];
@@ -89,6 +85,6 @@ abstract class WorkLog extends \yii\db\ActiveRecord
      */
     public function getProcess()
     {
-        return $this->hasOne(static::processClass(), ['id' => 'process_id']);
+        return $this->hasOne($this->processClass(), ['id' => 'process_id']);
     }
 }
