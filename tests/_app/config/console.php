dd3e844216d38b2d006eabb0e897465bd16e6c15
Angel Guevara <angeldelcaos@gmail.com>
2018-02-23 14:41:26 -0600

    fully enable yii2 debug

--- tests/_app/config/console.php
+++ tests/_app/config/console.php
@@ -1,23 +1,19 @@
 <?php
 
-return [
-    'id' => 'yii2-workflow-test',
-    'basePath' => dirname(__DIR__),
-    'aliases' => [
-        '@tests' => dirname(dirname(__DIR__)),
-        '@tecnocen/workflow' => dirname(dirname(dirname(__DIR__))) . '/src',
-        '@tecnocen/oauth2server' => VENDOR_DIR . '/tecnocen/yii2-oauth2-server/src',
-    ],
-    'components' => [
-        'log' => null,
-        'cache' => null,
-        'db' => require __DIR__ . '/db.php',
-    ],
-    'controllerMap' => [
-        'migrate' => [
-            'class' => yii\console\controllers\MigrateController::class,
-            'migrationPath' => null,
-            'migrationNamespaces' => [],
+return yii\helpers\ArrayHelper::merge(
+    require __DIR__ . '/common.php',
+    [
+        'id' => 'yii2-test-console',
+        'components' => [
+            'log' => null,
+            'cache' => null,
         ],
-    ],
-];
+        'controllerMap' => [
+            'migrate' => [
+                'class' => yii\console\controllers\MigrateController::class,
+                'migrationPath' => null,
+                'migrationNamespaces' => [],
+            ],
+        ],
+    ]
+);
